7d1fadd9815546a6468bdde0603ed46d19469c9e
David Levy <david.isaac.levy@gmail.com>
2026-07-29 20:12:34 -0400

    Add: minimum-stack - submission-5

---
+++ Data Structures & Algorithms/minimum-stack/submission-5.cs
@@ -0,0 +1,15 @@
+public class MinStack {
+    private Stack<(int Value, int Min)> _stack;
+
+    public MinStack() => _stack = new();
+
+    public void Push(int val) => _stack.Push((val, _stack.Count == 0
+                                                       ? val
+                                                       : Math.Min(val, _stack.Peek().Min)));
+
+    public void Pop() => _stack.Pop();
+
+    public int Top() => _stack.Peek().Value;
+
+    public int GetMin() => _stack.Peek().Min;
+}
